832840aa7334c368131bb2668fb51e2540fdb53d
Philipp Fehr <philipp@thefehr.ch>
2026-04-14 10:30:38 +0200

    chore(pocketbase-http): switch from single to double quotes for consistency

--- nodes/PocketbaseHttp/LoadOptions.ts
+++ nodes/PocketbaseHttp/LoadOptions.ts
@@ -1,135 +1,140 @@
-import { ILoadOptionsFunctions, INodePropertyOptions } from 'n8n-workflow';
+import { ILoadOptionsFunctions, INodePropertyOptions } from "n8n-workflow";
 
 export interface PocketBaseField {
-	autogeneratePattern?: string;
-	hidden: boolean;
-	id: string;
-	max?: number | string;
-	min?: number | string;
-	name: string;
-	pattern?: string;
-	presentable: boolean;
-	primaryKey?: boolean;
-	required?: boolean;
-	system: boolean;
-	type: string;
-	maxSelect?: number;
-	values?: string[];
-	onCreate?: boolean;
-	onUpdate?: boolean;
-	cascadeDelete?: boolean;
-	collectionId?: string;
-	minSelect?: number;
+  autogeneratePattern?: string;
+  hidden: boolean;
+  id: string;
+  max?: number | string;
+  min?: number | string;
+  name: string;
+  pattern?: string;
+  presentable: boolean;
+  primaryKey?: boolean;
+  required?: boolean;
+  system: boolean;
+  type: string;
+  maxSelect?: number;
+  values?: string[];
+  onCreate?: boolean;
+  onUpdate?: boolean;
+  cascadeDelete?: boolean;
+  collectionId?: string;
+  minSelect?: number;
 }
 
 async function loadPocketBaseFields(
-	this: ILoadOptionsFunctions,
-	collectionName: string | null = null,
+  this: ILoadOptionsFunctions,
+  collectionName: string | null = null,
 ): Promise<PocketBaseField[]> {
-	const { url } = await this.getCredentials('pocketbaseHttpApi');
-	const resource = collectionName
-		? collectionName
-		: (this.getNodeParameter('resource') as unknown as string);
-	const { fields } = await this.helpers.httpRequestWithAuthentication.call(this, 'pocketbaseHttpApi', {
-		url: `${url}/api/collections/${resource}`,
-		method: 'GET',
-	});
+  const { url } = await this.getCredentials("pocketbaseHttpApi");
+  const resource = collectionName
+    ? collectionName
+    : (this.getNodeParameter("resource") as unknown as string);
+  const { fields } = await this.helpers.httpRequestWithAuthentication.call(
+    this,
+    "pocketbaseHttpApi",
+    {
+      url: `${url}/api/collections/${resource}`,
+      method: "GET",
+    },
+  );
 
-	return fields as PocketBaseField[];
+  return fields as PocketBaseField[];
 }
 
 export const LoadOptions = {
-	async getCollections(this: ILoadOptionsFunctions): Promise<INodePropertyOptions[]> {
-		const { url } = await this.getCredentials('pocketbaseHttpApi');
+  async getCollections(this: ILoadOptionsFunctions): Promise<INodePropertyOptions[]> {
+    const { url } = await this.getCredentials("pocketbaseHttpApi");
 
-		const items: {name: string}[] = [];
-		let page: number = 1;
-		let totalPages: number = 1;
+    const items: { name: string }[] = [];
+    let page: number = 1;
+    let totalPages: number = 1;
 
-		do {
-			const { items: pageItems, totalPages: pageTotalPages } = await this.helpers.httpRequestWithAuthentication.call(
-				this,
-				'pocketbaseHttpApi',
-				{
-					url: `${url}/api/collections`,
-					method: 'GET',
-					qs: { page },
-				},
-			);
+    do {
+      const { items: pageItems, totalPages: pageTotalPages } =
+        await this.helpers.httpRequestWithAuthentication.call(this, "pocketbaseHttpApi", {
+          url: `${url}/api/collections`,
+          method: "GET",
+          qs: { page },
+        });
 
-			items.push(...pageItems);
-			if (totalPages === 1) {
-				totalPages = pageTotalPages;
-			}
-			page++;
-		} while (page <= totalPages);
+      items.push(...pageItems);
+      if (totalPages === 1) {
+        totalPages = pageTotalPages;
+      }
+      page++;
+    } while (page <= totalPages);
 
-		return items.map(({ name }) => ({ name, value: name })) as INodePropertyOptions[];
-	},
-	async getFields(this: ILoadOptionsFunctions): Promise<INodePropertyOptions[]> {
-		const returnData: INodePropertyOptions[] = [];
-		const fields = await loadPocketBaseFields.call(this);
+    return items.map(({ name }) => ({ name, value: name })) as INodePropertyOptions[];
+  },
+  async getFields(this: ILoadOptionsFunctions): Promise<INodePropertyOptions[]> {
+    const returnData: INodePropertyOptions[] = [];
+    const fields = await loadPocketBaseFields.call(this);
 
-		fields?.forEach(({ name, type }) => {
-			if (type === 'relation') {
-				returnData.push({
-					name: `All fields from relation '${name}'`,
-					value: `expand.${name}.*`
-				});
+    fields?.forEach(({ name, type }) => {
+      if (type === "relation") {
+        returnData.push({
+          name: `All fields from relation '${name}'`,
+          value: `expand.${name}.*`,
+        });
 
-				return;
-			}
+        return;
+      }
 
-			returnData.push({
-				name,
-				value: name,
-			});
-		});
+      returnData.push({
+        name,
+        value: name,
+      });
+    });
 
-		return returnData;
-	},
-	async getRelations(this: ILoadOptionsFunctions): Promise<INodePropertyOptions[]> {
-		const returnData: INodePropertyOptions[] = [];
-		const fields = await loadPocketBaseFields.call(this);
+    return returnData;
+  },
+  async getRelations(this: ILoadOptionsFunctions): Promise<INodePropertyOptions[]> {
+    const returnData: INodePropertyOptions[] = [];
+    const fields = await loadPocketBaseFields.call(this);
 
-		fields?.forEach(({ name, type }) => {
-			if (type !== 'relation') {
-				return;
-			}
-			returnData.push({
-				name,
-				value: name,
-			});
-		});
+    fields?.forEach(({ name, type }) => {
+      if (type !== "relation") {
+        return;
+      }
+      returnData.push({
+        name,
+        value: name,
+      });
+    });
 
-		return returnData;
-	},
-	async getRows(this: ILoadOptionsFunctions): Promise<INodePropertyOptions[]> {
-		const returnData: INodePropertyOptions[] = [];
-		const { url } = await this.getCredentials('pocketbaseHttpApi');
-		const resource = this.getNodeParameter('resource') as unknown as string;
-		const { items } = await this.helpers.httpRequestWithAuthentication.call(this, 'pocketbaseHttpApi', {
-			url: `${url}/api/collections/${resource}/records?sort=-created`,
-			method: 'GET',
-		});
+    return returnData;
+  },
+  async getRows(this: ILoadOptionsFunctions): Promise<INodePropertyOptions[]> {
+    const returnData: INodePropertyOptions[] = [];
+    const { url } = await this.getCredentials("pocketbaseHttpApi");
+    const resource = this.getNodeParameter("resource") as unknown as string;
+    const { items } = await this.helpers.httpRequestWithAuthentication.call(
+      this,
+      "pocketbaseHttpApi",
+      {
+        url: `${url}/api/collections/${resource}/records?sort=-created`,
+        method: "GET",
+      },
+    );
 
-		items?.forEach(({ id, ...data }: { id: string }) => {
-			const name = Object.entries(data).filter(([key]) => {
-				return key === 'name';
-			})?.[0]?.[1] as string | undefined;
-			const shortColumns = Object.fromEntries(
-				Object.entries(data).filter(([, column]) => {
-					const serialized = JSON.stringify(column);
-					return serialized.length <= 20 && serialized.length > 2;
-				}),
-			);
+    items?.forEach(({ id, ...data }: { id: string }) => {
+      const name = Object.entries(data).filter(([key]) => {
+        return key === "name";
+      })?.[0]?.[1] as string | undefined;
+      const shortColumns = Object.fromEntries(
+        Object.entries(data).filter(([, column]) => {
+          const serialized = JSON.stringify(column);
+          return serialized.length <= 20 && serialized.length > 2;
+        }),
+      );
 
-			returnData.push({
-				name: name ? name : JSON.stringify(shortColumns).substring(1, 100),
-				value: id,
-			});
-		});
+      returnData.push({
+        name: name ? name : JSON.stringify(shortColumns).substring(1, 100),
+        value: id,
+      });
+    });
 
-		return returnData;
-	},
+    return returnData;
+  },
 };
--- nodes/PocketbaseHttp/RequestBodyFunctions.ts
+++ nodes/PocketbaseHttp/RequestBodyFunctions.ts
@@ -1,71 +1,71 @@
 // eslint-disable-next-line @n8n/community-nodes/no-restricted-imports
-import FormData from 'form-data';
+import FormData from "form-data";
 import {
-	AssignmentCollectionValue,
-	IExecuteSingleFunctions,
-	IHttpRequestOptions,
-} from 'n8n-workflow';
+  AssignmentCollectionValue,
+  IExecuteSingleFunctions,
+  IHttpRequestOptions,
+} from "n8n-workflow";
 
 export async function prepareRequestBody(
-	this: IExecuteSingleFunctions,
-	requestOptions: IHttpRequestOptions,
+  this: IExecuteSingleFunctions,
+  requestOptions: IHttpRequestOptions,
 ) {
-	const bodyType = this.getNodeParameter('bodyType', ['parameters']) as string[];
+  const bodyType = this.getNodeParameter("bodyType", ["parameters"]) as string[];
 
-	const formData = new FormData();
+  const formData = new FormData();
 
-	if (bodyType.includes('fields')) {
-		(
-			this.getNodeParameter('fields', {
-				assignments: [],
-			}) as AssignmentCollectionValue
-		)?.assignments?.forEach(function ({ name, value }) {
-			formData.append(name, value);
-		});
-	}
+  if (bodyType.includes("fields")) {
+    (
+      this.getNodeParameter("fields", {
+        assignments: [],
+      }) as AssignmentCollectionValue
+    )?.assignments?.forEach(function ({ name, value }) {
+      formData.append(name, value);
+    });
+  }
 
-	if (bodyType.includes('bodyJson')) {
-			Object.entries(requestOptions.body).forEach(([key, value]) => {
-				formData.append(key, value);
-			});
-	}
+  if (bodyType.includes("bodyJson")) {
+    Object.entries(requestOptions.body).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+  }
 
-	if (bodyType.includes('binaryData')) {
-		await handleBinaryData.apply(this, [formData]);
-	}
+  if (bodyType.includes("binaryData")) {
+    await handleBinaryData.apply(this, [formData]);
+  }
 
-	if (!requestOptions.headers) {
-		requestOptions.headers = {};
-	}
-	requestOptions.headers['Content-Type'] = 'multipart/form-data';
+  if (!requestOptions.headers) {
+    requestOptions.headers = {};
+  }
+  requestOptions.headers["Content-Type"] = "multipart/form-data";
 
-	requestOptions.body = formData;
-	this.logger.info(`Request URL: ${requestOptions.url} | ${JSON.stringify(requestOptions.body)}`);
+  requestOptions.body = formData;
+  this.logger.info(`Request URL: ${requestOptions.url} | ${JSON.stringify(requestOptions.body)}`);
 
-	return requestOptions;
+  return requestOptions;
 }
 
 async function handleBinaryData(this: IExecuteSingleFunctions, formData: FormData) {
-	const binaryPropertyName = this.getNodeParameter('binaryPropertyName', undefined) as string;
+  const binaryPropertyName = this.getNodeParameter("binaryPropertyName", undefined) as string;
 
-	if (!binaryPropertyName) {
-		this.logger.info('No binary data to send. Skipping...');
-		return;
-	}
+  if (!binaryPropertyName) {
+    this.logger.info("No binary data to send. Skipping...");
+    return;
+  }
 
-	const binaryFieldName = this.getNodeParameter('binaryFieldName', undefined) as string;
-	this.logger.info(
-		'Adding binary data to request formData from property: ' +
-			binaryPropertyName +
-			'\nat: ' +
-			binaryFieldName,
-	);
+  const binaryFieldName = this.getNodeParameter("binaryFieldName", undefined) as string;
+  this.logger.info(
+    "Adding binary data to request formData from property: " +
+      binaryPropertyName +
+      "\nat: " +
+      binaryFieldName,
+  );
 
-	const binaryData = this.helpers.assertBinaryData(binaryPropertyName);
-	const dataBuffer = await this.helpers.getBinaryDataBuffer(binaryPropertyName);
+  const binaryData = this.helpers.assertBinaryData(binaryPropertyName);
+  const dataBuffer = await this.helpers.getBinaryDataBuffer(binaryPropertyName);
 
-	formData.append(binaryFieldName, dataBuffer, {
-		contentType: binaryData.mimeType,
-		filename: binaryData.fileName,
-	});
+  formData.append(binaryFieldName, dataBuffer, {
+    contentType: binaryData.mimeType,
+    filename: binaryData.fileName,
+  });
 }
